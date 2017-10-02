e9c1fdb4a072be79b1dbd0b5f468f38cbae1a2b6
rajesh_darak_cueris <rajesh.darak@cueris.com>
2017-10-02 14:02:00 +0530

    program 2 added

---
+++ Assignment5/program2/receiver.c
@@ -0,0 +1,34 @@
+#include "headerFiles.h"
+
+#define SHM_SIZE 27
+
+int main()
+{
+    int shmid;
+    key_t key;
+    char *shm, *s;
+
+    key = 5678;
+
+    if ((shmid = shmget(key, SHM_SIZE, 0666)) < 0)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1)
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+    for (s = shm; *s != NULL; s++)
+    {
+      putchar(*s);
+    }
+    putchar('\n');
+
+    *shm = '*';
+
+    exit(0);
+}
